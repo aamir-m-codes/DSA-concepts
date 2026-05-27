c06598a5e879c342318158cf7082000cc49ab715
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-05-27 10:33:49 +0500

    Added Binary Search Tree

---
+++ BST/BinarySearchTree.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+} Node;
+
+class BST
+{
+  Node *root;
+
+  void clear(Node *curr);
+  bool search_helper(Node *curr, int key);
+
+public:
+  BST();
+  BST(int data);
+  ~BST();
+
+  void insert(int data);
+  void search(int key);
+};
+
+void BST::clear(Node *curr)
+{
+  if (curr == nullptr)
+    return;
+
+  clear(curr->left);
+  clear(curr->right);
+
+  delete curr;
+}
+
+bool BST::search_helper(Node *curr, int key)
+{
+  if (curr == nullptr)
+    return false;
+
+  if (key == curr->data)
+    return true;
+  else if (key < curr->data)
+  {
+    return search_helper(curr->left, key);
+  }
+  else
+  {
+    return search_helper(curr->right, key);
+  }
+}
+
+BST::BST() : root(nullptr) {}
+
+BST::BST(int data) : BST()
+{
+  this->insert(data);
+}
+
+BST::~BST()
+{
+  this->clear(this->root);
+  this->root = nullptr;
+}
+
+void BST::insert(int data)
+{
+  if (this->root == nullptr)
+  {
+    Node *new_node = new Node{data, nullptr, nullptr};
+    this->root = new_node;
+    return;
+  }
+
+  Node *prev = nullptr;
+  Node *curr = this->root;
+
+  while (curr != nullptr)
+  {
+    prev = curr;
+    if (data == curr->data)
+    {
+      cout << "This data is already in BST" << endl;
+      return;
+    }
+    else if (data < curr->data)
+    {
+      curr = curr->left;
+    }
+    else
+    {
+      curr = curr->right;
+    }
+  }
+
+  Node *new_node = new Node{data, nullptr, nullptr};
+  if (data < prev->data)
+  {
+    prev->left = new_node;
+  }
+  else
+  {
+    prev->right = new_node;
+  }
+}
+
+void BST::search(int key)
+{
+  Node *curr = this->root;
+  bool is_found = this->search_helper(curr, key);
+  is_found ? cout << key << " is found" << endl : cout << key << " Not found in BST";
+}

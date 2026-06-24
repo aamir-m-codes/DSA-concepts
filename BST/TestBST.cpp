d2217350150b082eabfcb81da32e0a35fb9b5c68
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-06-24 08:39:10 +0500

    test deletion

--- BST/TestBST.cpp
+++ BST/TestBST.cpp
@@ -12,9 +12,12 @@ int main()
   bst.insert(70);
   bst.insert(9);
 
-  // it print in sorted order due to BST nature
+  // traversal take one argument of type int it expect only 0 -> inorder, 1 -> preorder, 2-> postorder for traversal
   bst.traversal();
   bst.search(70);
   bst.search(30);
+  bst.deletion(12);
+  cout << "After deletion: ";
+  bst.traversal();
   return 0;
 }
